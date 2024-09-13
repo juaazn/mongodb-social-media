5d1f68d5ec3d3a23183754dbaf778ae8f22ba9c4
chuans <juaazn.05@gmail.com>
2024-09-13 13:30:38 +0200

    Cloudinary controller

---
+++ controllers/CloudinaryController.js
@@ -0,0 +1,14 @@
+const CloudinaryController = {
+  async upload (req, res) {
+    try {
+      if (!req.file) return res.status(400).json({ message: 'No se subió ningún archivo' })
+
+      res.status(200).json({ message: 'Actualizado', url: req.file })
+    } catch (error) {
+      console.error('Error en el controlador Cloudinary: ', error)
+      res.status(500).send({ message: 'Error cloudinary internal server: ', error })
+    }
+  }
+}
+
+export default CloudinaryController
