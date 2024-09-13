3909d6e6113fe42361dea4a1c4cf63045338e036
chuans <juaazn.05@gmail.com>
2024-09-13 13:30:11 +0200

    route cloudinary

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dbConnection from '../config/config.js'
 import post from '../router/post.js'
 import user from '../router/user.js'
 import comments from '../router/comments.js'
+import cloudinary from '../router/cloudinary.js'
 import { typeError } from '../middlewares/errors.js'
 
 const app = express()
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use('/api/post', post)
 app.use('/api/user', user)
 app.use('/api/comments', comments)
+app.use('/api/cloudinary', cloudinary)
 
 app.use(typeError)
 
