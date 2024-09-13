968198b03fc193651dad0f58758c5731449a1dec
chuans <juaazn.05@gmail.com>
2024-09-13 13:30:25 +0200

    Cloudinary config

---
+++ cloudinary/config.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import multer from 'multer';
+
+cloudinary.config({
+  account_id: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'ecommerce',
+  }
+})
+
+export const upload =  multer({ storage })
