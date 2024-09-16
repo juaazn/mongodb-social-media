59cca0a10e7fe6c6803c783fd9d8b2ffc7fca2eb
chuans <juaazn.05@gmail.com>
2024-09-16 20:16:08 +0200

    fix

--- controllers/CloudinaryController.js
+++ controllers/CloudinaryController.js
@@ -2,7 +2,6 @@ const CloudinaryController = {
   async upload (req, res) {
     try {
       if (!req.file) return res.status(400).send({ message: 'No se subió ningún archivo' })
-
       res.status(200).json({ message: 'Actualizado', url: req.file })
     } catch (error) {
       console.error('Error en el controlador Cloudinary: ', error)
