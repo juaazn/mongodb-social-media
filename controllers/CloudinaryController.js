460765dacaaf587fac018cce45c042c48ec0efe0
chuans <juaazn.05@gmail.com>
2024-09-16 19:30:12 +0200

    refactor

--- controllers/CloudinaryController.js
+++ controllers/CloudinaryController.js
@@ -1,12 +1,12 @@
 const CloudinaryController = {
   async upload (req, res) {
     try {
-      if (!req.file) return res.status(400).json({ message: 'No se subió ningún archivo' })
+      if (!req.file) return res.status(400).send({ message: 'No se subió ningún archivo' })
 
       res.status(200).json({ message: 'Actualizado', url: req.file })
     } catch (error) {
       console.error('Error en el controlador Cloudinary: ', error)
-      res.status(500).send({ message: 'Error cloudinary internal server: ', error })
+      res.status(500).send({ message: `Error cloudinary internal server: ${error}` })
     }
   }
 }
