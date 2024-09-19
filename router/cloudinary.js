705da2155b499844b1066dc07ce038a2a69bbd30
chuans <juaazn.05@gmail.com>
2024-09-19 15:52:31 +0200

    change upload cloudinary

--- router/cloudinary.js
+++ router/cloudinary.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { authentication } from '../middlewares/authentication.js'
-import { upload } from '../cloudinary/config.js'
+import { upload } from '../utils/cloudinary.js'
 import CloudinaryController from '../controllers/CloudinaryController.js'
 
 const cloudinary = express.Router()
