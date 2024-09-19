826a3c74ca72550bb65cc4faab0cd3d8b2d6f692
chuans <juaazn.05@gmail.com>
2024-09-19 14:52:33 +0200

    Endpoint aupdate

--- router/post.js
+++ router/post.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import PostController from '../controllers/PostControllers.js'
+import { upload } from '../cloudinary/config.js'
 
 import { authentication, isAuthor } from '../middlewares/authentication.js'
 import { typeError } from '../middlewares/errors.js'
@@ -9,7 +10,7 @@ const posts = express.Router()
 posts.get('/getAll', PostController.getAll)
 posts.get('/id/:_id', PostController.getById)
 posts.get('/name/:_id', PostController.getPostByName)
-posts.post('/create/:_id', authentication,typeError, PostController.create)
+posts.post('/create/:_id', upload.single('picture'), authentication,typeError, PostController.create)
 posts.post('/like/:_id', authentication, PostController.like)
 posts.put('/update/:_id', authentication, isAuthor, PostController.update)
 posts.delete('/delete/:_id', authentication, isAuthor, PostController.delete)
