9c0fbf0585a03787a1b8cf2a2a232802e191cdcd
chuans <juaazn.05@gmail.com>
2024-09-19 14:54:44 +0200

    Authentication

--- router/cloudinary.js
+++ router/cloudinary.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import { authentication } from '../middlewares/authentication.js'
 import { upload } from '../cloudinary/config.js'
 import CloudinaryController from '../controllers/CloudinaryController.js'
 
 const cloudinary = express.Router()
 
-cloudinary.post('/image', upload.single('picture'), CloudinaryController.upload)
+cloudinary.post('/image', authentication, upload.single('picture'), CloudinaryController.upload)
 
 export default cloudinary
