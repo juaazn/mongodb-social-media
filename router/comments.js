2d8913d714d848c6124b3b918ad2600a18fd62db
chuans <juaazn.05@gmail.com>
2024-09-19 16:54:42 +0200

    CRUD

--- router/comments.js
+++ router/comments.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import Comments from '../controllers/CommentsControllers.js'
-import { authentication } from '../middlewares/authentication.js'
+import { authentication, isAuthorComments } from '../middlewares/authentication.js'
 
 const comments = express.Router()
 
 comments.post('/create/:_id', authentication, Comments.create)
+comments.put('/edit/:_id', authentication, isAuthorComments, Comments.editComments)
+comments.get('/get', Comments.getAll)
+comments.delete('/delete/:_id', authentication, isAuthorComments, Comments.delete)
 
 export default comments
