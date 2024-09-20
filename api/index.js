1f13335b6e46bfd80dd08fdf4dc6d04ec8134fae
chuans <juaazn.05@gmail.com>
2024-09-21 00:47:22 +0200

    cors added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express from 'express'
 import dbConnection from '../config/config.js'
+import cors from 'cors'
 import post from '../router/post.js'
 import user from '../router/user.js'
 import comments from '../router/comments.js'
@@ -9,6 +10,7 @@ import { typeError } from '../middlewares/errors.js'
 
 const app = express()
 dbConnection()
+app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
