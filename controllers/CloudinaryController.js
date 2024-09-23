e72feb73859d28ad062ec8d8d2c5a3babcf5459c
chuans <juaazn.05@gmail.com>
2024-09-23 20:06:58 +0200

    Added phote to user

--- controllers/CloudinaryController.js
+++ controllers/CloudinaryController.js
@@ -1,8 +1,16 @@
+import User from '../models/User.js'
+
 const CloudinaryController = {
   async upload (req, res) {
     try {
       if (!req.file) return res.status(400).send({ message: 'No se subió ningún archivo' })
+      const _id = req.user._id
 
+      const user = await User.findById(_id)
+      if (!user) return res.status(400).send({ message: 'User not found' })
+
+      user.profileImage = req.file
+      user.save()
       res.status(200).json({ message: 'Actualizado', url: req.file })
     } catch (error) {
       console.error('Error en el controlador Cloudinary: ', error)
