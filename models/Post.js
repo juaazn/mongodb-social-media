d07e54e18cf9fbbd419089019f9646f31344da20
chuans <juaazn.05@gmail.com>
2024-09-29 00:44:48 +0200

    model post

--- models/Post.js
+++ models/Post.js
@@ -1,21 +1,8 @@
 import mongoose from 'mongoose'
-const ObjectId = mongoose.SchemaTypes.String
-const userName = mongoose.SchemaTypes.String
 
 const PostSchema = new mongoose.Schema(
   {
-    userId: {
-      type: ObjectId,
-      ref: 'Users'
-    },
-    userName: {
-      type: userName,
-      ref: 'Users',
-    },
-    title: {
-      type: String,
-      required: [true, 'Por favor escriba un titulo'],
-    },
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'Users'},
     image: {
       type: Map,
       of: String,
@@ -26,7 +13,7 @@ const PostSchema = new mongoose.Schema(
       required: [true, 'Tiene que escribir algo, para poder publicar un post'],
     },
     comments: [],
-    like: [ { userId: {type: ObjectId, ref: 'User'}, userName: String, isLike: Boolean } ]
+    like: [{type: mongoose.Types.ObjectId, ref: 'User'}]
   },
   { timestamps: true }
 )
