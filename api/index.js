87246c4723b69fd6b7b9537b25704591279c1f38
chuans <juaazn.05@gmail.com>
2024-11-07 14:30:06 +0100

    urlencoded for utf-8

--- api/index.js
+++ api/index.js
@@ -12,6 +12,7 @@ const app = express()
 dbConnection()
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 3000
 
 app.use('/api/post', post)
